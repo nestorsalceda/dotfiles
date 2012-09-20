76a605a4a5fca13a13b1557023bb5887e19886ba
Néstor Salceda <nestor.salceda@gmail.com>
2012-09-21 00:20:08 +0200

    Enable oh-my-zsh plugin for virtualenvwrapper

--- zsh/virtualenvwrapper.zsh
+++ zsh/virtualenvwrapper.zsh
@@ -1,9 +1,4 @@
-WORKON_HOME=~/.virtualenvs
-export WORKON_HOME
-
 VIRTUAL_ENV_DISABLE_PROMPT=0
-
-source /etc/bash_completion.d/virtualenvwrapper
 
 function virtualenv_info {
     [ $VIRTUAL_ENV ] && echo $(basename $VIRTUAL_ENV)
