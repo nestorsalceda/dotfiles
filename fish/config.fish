b6c8f5e5397f78b0442db08a5edb53e162c8e20d
Néstor Salceda <nestor.salceda@gmail.com>
2017-12-10 02:30:19 +0100

    Use fish variable instead of $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,4 @@ set fish_key_bindings fish_vi_key_bindings
 set -x EDITOR "vim"
 set -x VISUAL "vim"
 set -x GOPATH ~/Projects/golang
-set -x PATH $GOPATH/bin $PATH
+set -U fish_user_paths $GOPATH/bin ~/.local/bin $fish_user_paths
