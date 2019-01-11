4011bce5b0ff7ff3fe078a01d6d932603662861c
Néstor Salceda <nestor.salceda@gmail.com>
2019-01-11 20:46:21 +0100

    Enable kubectl completion

--- zsh/.zsh/completion.zsh
+++ zsh/.zsh/completion.zsh
@@ -18,3 +18,7 @@ zstyle ':completion:*' verbose true
 
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+if [ $commands[kubectl] ]; then
+  source <(kubectl completion zsh)
+fi
