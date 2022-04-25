758e360dce2b4f1497ec029fcd14710b268d5806
Néstor Salceda <nestor.salceda@gmail.com>
2022-04-25 13:30:38 +0200

    feat: Enable ruby LSP server

--- nvim/.config/nvim/after/plugin/lspconfig.lua
+++ nvim/.config/nvim/after/plugin/lspconfig.lua
@@ -36,7 +36,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'gopls', 'jsonls', 'yamlls' }
+local servers = { 'gopls', 'jsonls', 'yamlls', 'solargraph' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
