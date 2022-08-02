b9afb530c3183485cca9fcbea35f4895feee4672
Néstor Salceda <nestor.salceda@gmail.com>
2022-08-02 12:01:32 +0200

    Remove GEM_HOME variable

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,4 +1,3 @@
 export GOPATH=$HOME/.go
-export GEM_HOME=$HOME/.gems
 
-export PATH=$HOME/bin:$GOPATH/bin:$GEM_HOME/bin:$HOME/.local/bin:$PATH
+export PATH=$HOME/bin:$GOPATH/bin:$HOME/.local/bin:$PATH
