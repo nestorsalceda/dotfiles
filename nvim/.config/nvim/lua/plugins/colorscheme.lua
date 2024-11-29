a6b2f98e07535677b16506e5d03afa14f68f1148
Néstor Salceda <nestor.salceda@gmail.com>
2024-11-29 12:00:53 +0100

    Use catpuccin theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-  -- add dracula
-  { "Mofiqul/dracula.nvim" },
+  { "catppuccin/nvim" },
+  lazy = false,
+  name = "catppuccin",
 
-  -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "catppuccin",
     },
   },
 }
