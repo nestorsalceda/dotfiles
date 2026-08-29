88b28f56eb9a0e3c2566aa9f84bd9518ec81eb78
Néstor Salceda <nestor.salceda@gmail.com>
2026-08-29 09:31:18 +0200

    change: Add the `fzf` integration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,20 +4,23 @@ set -g fish_key_bindings fish_vi_key_bindings
 set -gx GOPATH $HOME/.go
 fish_add_path $GOPATH/bin $HOME/bin $HOME/.local/bin
 
-set -U EDITOR nvim
-set -U VISUAL nvim
-
-if type -q eza
-    alias ls eza
-else
-    alias ls 'ls --color'
-end
-
-alias g git
-alias k kubectl
+set -gx EDITOR nvim
+set -gx VISUAL nvim
 
 fish_config theme choose catppuccin-frappe
 
-starship init fish | source
-direnv hook fish | source
-zoxide init fish | source
+if status is-interactive
+    if type -q eza
+        alias ls eza
+    else
+        alias ls 'ls --color'
+    end
+
+    alias g git
+    alias k kubectl
+
+    starship init fish | source
+    direnv hook fish | source
+    zoxide init fish | source
+    fzf --fish | source
+end
